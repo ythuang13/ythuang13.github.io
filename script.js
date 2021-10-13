fa95c4e5cb15d0f1791d5c0499014ca58d10c3c5
ythuang13 <ythuang13@users.noreply.github.com>
2021-10-12 17:25:40 -0700

    random dog pic

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function setTitle() {
-  //document.title = "Rickroll"
-}
-
-window.onload = setTitle;
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('https://dog.ceo/api/breeds/image/random', {
+        method : 'GET',
+        mode: 'cors'
+    })
+    .then(response => response.json())
+    .then(data => {
+        const dog = document.getElementById('dog');
+        dog.innerHTML = `<img src="${data['message']}">`;
+    })
+    .catch (error => {
+        console.log("Error: ", error);
+    })
+});
